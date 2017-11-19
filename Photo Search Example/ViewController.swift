cdb237af57edcfff86b1ccf8c4f64df717eafa3d
johnrnyquist <john@nyquist.net>
2017-11-19 05:59:38 -0700

    First commit.

--- Photo Search Example/ViewController.swift
+++ Photo Search Example/ViewController.swift
@@ -10,9 +10,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let manager = AFHTTPSessionManager()
+        
+        let searchParameters:[String:Any] = ["method": "flickr.photos.search",
+                                             "api_key": "713bc93d1d496ba2746a3b28c96e3892",
+                                             "format": "json",
+                                             "nojsoncallback": 1,
+                                             "text": "dogs",
+                                             "extras": "url_m",
+                                             "per_page": 5]
+        
+        manager.get("https://api.flickr.com/services/rest/",
+                    parameters: searchParameters,
+                    progress: nil,
+                    success: { (operation: URLSessionDataTask, responseObject:Any?) in
+                        if let responseObject = responseObject as? [String: AnyObject] { //unwrap the optional
+                            print(responseObject)
+                           if let photos = responseObject["photos"] as? [String: AnyObject] { //dictionary
+                                if let photoArray = photos["photo"] as? [[String: AnyObject]] { //array of dictionaries
+                                    self.scrollView.contentSize = CGSize(width: 320, height: 320 * CGFloat(photoArray.count))
+
+                                        for (i, photoDictionary) in photoArray.enumerated() {                             //1
+                                        
+                                            if let imageURLString = photoDictionary["url_m"] as? String {               //2
+                                                
+                                                let imageData = NSData(contentsOf: URL(string: imageURLString)!)        //2
+                                                
+                                                if let imageDataUnwrapped = imageData {                                     //3
+                                                    
+                                                    let imageView = UIImageView(image: UIImage(data: imageDataUnwrapped as Data))   //4
+                                                    imageView.frame = CGRect(x: 0, y: 320 * CGFloat(i), width: 320, height: 320) //5
+                                                    self.scrollView.addSubview(imageView)                                   //6
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                        }
+        }) { (operation:URLSessionDataTask?, error:Error) in
+            print("Error: " + error.localizedDescription)
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +62,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
 
 }
 
---
+++ bridge.h
@@ -0,0 +1,16 @@
+//
+//  bridge.h
+//  Web Service Example
+//
+//  Created by John Nyquist on 11/14/17.
+//  Copyright © 2017 Nyquist Art + Logic LLC. All rights reserved.
+//
+
+#ifndef bridge_h
+#define bridge_h
+
+#import "Photo Search Example/AFNetworking/AFNetworking.h"
+#import "UIKit+AFNetworking/UIKit+AFNetworking.h"
+
+
+#endif /* bridge_h */
